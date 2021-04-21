1289d16cd3381bef28e288252f646e3e5cc8d174
Yarik-Yashin <yarik101105@yandex.ru>
2021-04-21 22:51:42 +0300

    таблица лидеров капец как круто

--- data/casino_api.py
+++ data/casino_api.py
@@ -1,6 +1,8 @@
 import flask
 from flask import request, make_response, render_template
-from flask_login import current_user
+from flask_login import current_user, login_required
+from . import db_session
+from .users import User
 
 blueprint = flask.Blueprint(
     'casino',
@@ -14,6 +16,15 @@ def index():
     return render_template('index.html')
 
 
+@login_required
 @blueprint.route('/profile', methods=['GET'])
 def profile():
     return render_template('profile.html', current_user=current_user)
+
+
+@login_required
+@blueprint.route('/leaders', methods=['GET'])
+def leaders():
+    session = db_session.create_session()
+    users = session.query(User).order_by(User.money).all()[::-1]
+    return render_template('leaders.html', users=users)
